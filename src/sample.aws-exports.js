9a70a0f68def2ae4bb297f3e933f7a2324d14246
Ashish <ashish.mainali@ymail.com>
2023-07-28 23:53:02 -0400

    Adding amplify backend

--- src/sample.aws-exports.js
+++ src/sample.aws-exports.js
@@ -1,23 +1,22 @@
-// note that Auth is top-level, whereas the API stuff is not
 const awsmobile = {
-	Auth: {
-		region: 'your-region',
-		userPoolId: 'your-region_YOUR_REGION_ID',
-		userPoolWebClientId: '6vstsuciYOURCLIENTID',
-		identityPoolId: 'identitypoolID',
-		
-	},
-	Storage: {
-		AWSS3: {
-			bucket: 'bucket-name',
-			region: 'your-region',
-		},
-	},
-	aws_project_region: 'your-region',
-	aws_appsync_graphqlEndpoint:
-		'https://your-api.appsync-api.your-region.amazonaws.com/graphql',
-	aws_appsync_region: 'your-region',
-	aws_appsync_authenticationType: 'AMAZON_COGNITO_USER_POOLS',
+    Auth: {
+        region: 'us-east-1',
+        userPoolId: 'us-east-1_7AFoI4FRG',
+        userPoolWebClientId: 'ggigbfv929tfoik4ss8a8nhog',
+        identityPoolId: 'us-east-1:45a93150-828c-4923-8f75-eda7c0da68c8',
+    },
+    Storage: {
+        AWSS3: {
+            bucket: 'filestoragestack-s3bucket64cb25af-1gkb843m1654d',
+            region: 'us-east-1',
+        },
+    },
+    aws_project_region: 'us-east-1',
+    aws_appsync_graphqlEndpoint:
+        'https://gcajw4nblrfjzk5473lklxrhcu.appsync-api.us-east-1.amazonaws.com/graphql',
+    aws_appsync_region: 'us-east-1',
+    aws_appsync_authenticationType: 'AMAZON_COGNITO_USER_POOLS',
 }
 
 export default awsmobile
+
